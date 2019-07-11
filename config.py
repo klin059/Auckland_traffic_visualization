85f9ecc4a363ac9f8631c438b5482e8ce38b2774
Kuan-Min Lin <17500820+klin059@users.noreply.github.com>
2019-07-11 16:44:36 +1200

    change gitignore

---
+++ config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 15:20:32 2019
+
+@author: KML
+"""
+
+MAPBOX_KEY = 'pk.eyJ1Ijoia2xpbjA1OSIsImEiOiJjanh3cjh2ZjYwOG1sM2xwYjM3Y3h0NzNwIn0.xpsBQqqKa3rdri09fsJ1OQ'
